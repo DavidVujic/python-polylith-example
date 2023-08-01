7954de0cb8bea2f988a458b4876d3b8a31a02b86
David Vujic <david@vujic.se>
2023-08-01 11:25:24 +0200

    wip: add example code for Kafka producer and consumer

--- components/demo/message/__init__.py
+++ components/demo/message/__init__.py
@@ -1,3 +1,3 @@
-from demo.message.core import create, read, update, delete
+from demo.message.core import create, read, update, delete, notify
 
-__all__ = ["create", "read", "update", "delete"]
+__all__ = ["create", "read", "update", "delete", "notify"]
---
+++ test/components/demo/dictionaries/test_core.py
@@ -0,0 +1,5 @@
+from demo.dictionaries import core
+
+
+def test_sample():
+    assert core is not None
