ab78a4b202d309f913cb2499d3ac63a3e6e8ea04
David Vujic <david@vujic.se>
2025-03-09 09:49:10 +0100

    add jupyter to dev dependencies

--- development/david.py
+++ development/david.py
@@ -1,6 +1,17 @@
-from example import log, greeting
+from example import greeting, log
+from example.greet_api import core
+from uvicorn import Config, Server
+import asyncio
 
 logger = log.get_logger("The DEV logger")
 data = greeting.hello_world()
 
 logger.info(data)
+
+config = Config(core.app, host="127.0.0.1", port=8000)
+server = Server(config)
+
+def start_local():
+    loop = asyncio.get_running_loop()
+
+    loop.create_task(server.serve())
