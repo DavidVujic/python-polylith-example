107a499084c293f6a525127010505e59dd594eb7
David Vujic <david@vujic.se>
2025-03-26 21:17:23 +0100

    add RDD Dev-only method to start a FastAPI app in an event loop

--- development/david.py
+++ development/david.py
@@ -1,17 +1,31 @@
 from example import greeting, log
 from example.greet_api import core
-from uvicorn import Config, Server
-import asyncio
 
 logger = log.get_logger("The DEV logger")
 data = greeting.hello_world()
 
 logger.info(data)
 
-config = Config(core.app, host="127.0.0.1", port=8000)
-server = Server(config)
 
-def start_local():
+def start_local_for_repl_driven_development_with_jupyter_kernel():
+    """Dev-only FastAPI startup.
+    Important: only for REPL Driven Development using Jupyter kernel.
+
+    This will start the app in an event loop. This is useful
+    when running the app in a Jupyter kernel and connecting clients
+    to the kernel.
+    """
+
+    import asyncio
+
+    from example.message_api import core
+    from uvicorn import Config, Server
+
+    config = Config(core.app, host="127.0.0.1", port=8000)
+    server = Server(config)
+
     loop = asyncio.get_running_loop()
 
     loop.create_task(server.serve())
+
+    return loop
