b31dfeece97b251c37ca6d394d65d4d935a1231c
David Vujic <david@vujic.se>
2025-08-31 14:15:12 +0200

    testing out the deps save option

--- components/example/database/core.py
+++ components/example/database/core.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+from example import dictionaries
+
+
 DATABASE_URL = "sqlite:///./sql_app.db"
 DATABASE_ARGS = {"check_same_thread": False}
 
@@ -9,3 +12,7 @@ engine = create_engine(DATABASE_URL, connect_args=DATABASE_ARGS)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def comment():
+    dictionaries.pick({"hello": "world", "hi": "polylith"}, {"hi"})
